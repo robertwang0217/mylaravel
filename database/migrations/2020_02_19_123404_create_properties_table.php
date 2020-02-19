5f204b8dfc35aed4a8b6467e95b59235a1e1d76b
Robert Wang <robertwang0217@gmail.com>
2020-02-19 23:49:37 +1100

    migration files to create required tables

---
+++ database/migrations/2020_02_19_123404_create_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('suburb');
+            $table->string('state');
+            $table->string('country');
+            $table->uuid('guid');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('properties');
+    }
+}
---
+++ database/migrations/2020_02_19_123449_create_property_analytics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePropertyAnalyticsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('property_analytics', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->integer('property_id')->unique();
+            $table->integer('analytic_type_id')->unique();
+            $table->text('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('property_analytics');
+    }
+}
