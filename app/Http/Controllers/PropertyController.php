185bee7fd142bcfb3e89e369a8b75c81d8112195
Robert Wang <robertwang0217@gmail.com>
2020-02-20 11:53:49 +1100

    Api for create property

--- app/Http/Controllers/PropertyController.php
+++ app/Http/Controllers/PropertyController.php
@@ -7,7 +7,20 @@ use App\Models;
 
 class PropertyController extends Controller
 {
-    public function create() {
-    		
+    public function create( Request $request ) {
+
+    	$property = Models\Property::create( $request->all() );
+
+    	if( !$property->id ) {
+    		return response()->json([
+    		    'success'  => false,
+    		    'message' => 'Fail to create'
+    		], 422);
+    	}
+
+    	return response()->json([
+    	    'success'  => true,
+    	    'data' => $property
+    	]);
     }
 }
